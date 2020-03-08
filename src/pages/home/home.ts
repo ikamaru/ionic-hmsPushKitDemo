dedc538b4a2be59ed8928da581c5a41a6f7ab920
ikamaru <ikamaru.ikuane@gmail.com>
2020-03-08 18:10:25 +0100

    init the demo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,64 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
+//TODO 1
+declare var cordova;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+ 	constructor(private loadingController:LoadingController) {}
+ 	
 
-  }
+ 	async getToken(){
+ 		const loading = await this.loadingController.create({
+	      content: 'get token ...'
+	    });
+	    await loading.present();
+		cordova.plugins.HMSPushKit.getToken(
+			(token) => {
+				console.log(token);
+		    	alert(token);
+		    	loading.dismiss();
+			},(err) => {
+		    	alert(err);
+			    loading.dismiss();
+			});
+ 	}
+ 	async checkHMS(){
+ 		const loading = await this.loadingController.create({
+	      content: 'Check HMS ...'
+	    });
+	    await loading.present();
+
+		cordova.plugins.HMSPushKit.isHMSAvailable(
+			(msg) => {
+			    console.log(msg);
+			    alert(msg);
+			    loading.dismiss();
+			},(err) => {
+			    alert(err);
+			    loading.dismiss();
+			});
+ 	}
+ 	async checkGMS(){
+ 		const loading = await this.loadingController.create({
+	      content: 'Check GMS ...'
+	    });
+	    await loading.present();
+		cordova.plugins.HMSPushKit.isGMSAvailable(
+			(msg) => {
+			    console.log(msg);
+			    alert(msg);
+			    loading.dismiss();
+			},(err) => {
+			    alert(err);
+			    loading.dismiss();
+			});
+ 	}
 
 }
